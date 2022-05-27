31e9ed8d996cf769d1b8058e22ee2b5deb34fca7
Renan Verzinhassi <rverzinhassi@gmail.com>
2022-05-27 14:56:18 -0300

    Primeiro commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import "./Header.css"
+
+const Header = () => {
+    return <h1 className='header'>- Minhas Tarefas -</h1>;
+}
+ 
+export default Header;
---
+++ src/components/Task.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {CgClose} from 'react-icons/cg'
+
+import "./Task.css";
+
+const Task = ({ task, handleTaskClick, handleTaskDeletion }) => {
+    return (
+        <div
+            className="task-container"
+            style={task.completed ? { background: "chartreuse", color: '#444' } : {}}
+        >
+            <div className="task-title" onClick={() => handleTaskClick(task.id)}>
+                {task.title}
+            </div>
+            <div className="buttons-container">
+                <button
+                className='remove-task-button'
+                onClick={() => handleTaskDeletion(task.id)}
+                >
+                    <CgClose />
+                </button>
+            </div>
+        </div>
+    );
+}
+ 
+export default Task;
